b1716249d0ff5931f3465ef5c8600c8658b0eb08
unknown <13jmw28@queensu.ca>
2018-03-11 18:46:18 -0400

    add php

---
+++ register.php
@@ -0,0 +1,60 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$passworddb = "";
+$dbname = "theatrecomplexdb"; 
+$fname = $_POST["fname"]; 
+$lname = $_POST["lname"]; 
+$email = $_POST["email"]; 
+$phonenumber = $_POST["phone_number"]; 
+$password =  $_POST["password"];
+$confirmpassword = $_POST["password2"]; 
+$streetnumber = $_POST["street_number"]; 
+$streetname = $_POST["street_name"]; 
+$postalcode = $_POST["postal_code"]; 
+$nameoncard = $_POST["cc_name"]; 
+$creditcardnumber = $_POST["cc_number"];  
+$expiration = $_POST["cc_exp"];            
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $passworddb, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+ 
+$sql = "select max(member_id) from member";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$nextmemberid = mysqli_fetch_array($result)['max(member_id)'] + 1;
+}
+
+else {
+    $nextmemberid = 1;
+}
+
+
+
+ 
+$sql = "insert into User_Account (email, _password, fname, lname, street_name, street_num,
+postal_code, phone_number) values
+('$email','$password','$fname', '$lname','$streetname',$streetnumber,'$postalcode', '$phonenumber');";
+
+
+
+
+$sql .= "insert into member (member_id, email, card_number, card_expiry) values
+($nextmemberid, '$email','$creditcardnumber','$expiration')";
+
+
+
+if ($conn->multi_query($sql) === TRUE) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+?>
